8fbdc90709e3bab2c938e2cc35e3c43377b67574
Jordan Giacone <Jmgiacone@gmail.com>
2014-03-01 00:20:44 -0600

    Added skeleton code. Hope it compiles

---
+++ hw3/linkedlist.hpp
@@ -0,0 +1,77 @@
+template <typename T>
+int LinkedList<T>::size() const
+{
+  return 0;
+}
+
+template <typename T>
+bool LinkedList<T>::isEmpty() const
+{
+  return false;
+}
+
+template <typename T>
+LinkedList<T>* LinkedList<T>::getFirstPtr()
+{
+  return NULL;
+}
+
+template <typename T>
+LinkedList<T>* LinkedList<T>::getLastPtr()
+{
+  return NULL;
+}
+
+template <typename T>
+LinkedList<T>* LinkedList<T>::getAtPtr(int i)
+{
+  return NULL;
+}
+
+template <typename T>
+void LinkedList<T>::clear()
+{
+
+}
+
+template <typename T>
+voad LinkedList<T>::insert_front(const T& x)
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::insert(const T& x, LinkedList<T>* pos)
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::remove(LinkedList<T>* pos)
+{
+
+}
+
+template <typename T>
+bool LinkedList<T>:: operator == (const LinkedList<T>& rhs) const
+{
+  return false;
+}
+
+template <typename T>
+LinkedList<T>* LinkedList<T>::find(const T& x)
+{
+  return NULL;
+}
+
+template <typename T>
+void LinkedList<T>::reverse()
+{
+
+}
+
+template <typename T>
+void LinkedList<T>::append(const LinkedList<T> & xlist)
+{
+
+}
